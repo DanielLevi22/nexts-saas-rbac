51e6d8b6578329dfadfff9a8a2fe5a258cd59b29
Daniel Levi <daniellevi000@gmail.com>
2024-04-13 09:27:12 -0300

    feat: create function define Roles

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,8 @@
-import { ability } from '@saas/auth'
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'MEMBER' })
 
 const userCanntInviteSemenoeElse = ability.can('invite', 'User')
 const deletes = ability.can('delete', 'User')
 
-console.log(deletes)
+console.log(deletes, userCanntInviteSemenoeElse)
