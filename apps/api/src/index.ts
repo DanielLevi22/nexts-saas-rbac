09f5df2c66d1235fec26f00dd9725a00add436e6
Daniel Levi <daniellevi000@gmail.com>
2024-04-25 12:45:02 -0300

    feat: create schema tables prisma

--- apps/api/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER' })
-
-const userCanntInviteSemenoeElse = ability.can('invite', 'User')
-const deletes = ability.can('delete', 'User')
-
-console.log(deletes, userCanntInviteSemenoeElse)
