e81b997d6254250b5bce98f80e37e08056c344dd
Daniel Levi <daniellevi000@gmail.com>
2024-04-25 21:49:01 -0300

    feat: add swagger in project and create document route createUser

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,7 +1,9 @@
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -14,7 +16,21 @@ app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js Saas',
+      description: 'Full-stack Saas app with multi-tenant & RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
 
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 app.register(createAccount)
 app.listen({ port: 3333 }).then(() => {
   console.log('listening on port 3333')
