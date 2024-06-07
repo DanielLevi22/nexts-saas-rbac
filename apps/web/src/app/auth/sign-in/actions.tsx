8c2ae5de90166ff102f84754d9a472531c899579
Daniel Levi <daniellevi000@gmaiil.com>
2024-06-07 16:03:51 -0300

    feat:create an request form

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,18 +1,13 @@
 'use server'
-import ky from 'ky'
-const api = ky.create({
-  prefixUrl: 'http://localhost:3333',
-})
+
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
 export async function signInWithEmailAndPassword(data: FormData) {
   const { email, password } = Object.fromEntries(data)
-  const result = await api
-    .post('sessions/password', {
-      json: {
-        email,
-        password,
-      },
-    })
-    .json()
 
+  const result = await signInWithPassword({
+    email: email.toString(),
+    password: password.toString(),
+  })
   console.log(result)
 }
