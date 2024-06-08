a36c78b382e2737eb8a5da72f8702c76289b5e84
Daniel Levi <daniellevi000@gmaiil.com>
2024-06-07 23:34:23 -0300

    feat: crete form actions sign in

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,13 +1,43 @@
 'use server'
 
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
-export async function signInWithEmailAndPassword(data: FormData) {
-  const { email, password } = Object.fromEntries(data)
+const signInSchema = z.object({
+  email: z.string({ message: 'Please, provide a valid ee-mail address.' }),
+  password: z.string().min(1, { message: 'Please, provide a password.' }),
+})
 
-  const result = await signInWithPassword({
-    email: email.toString(),
-    password: password.toString(),
-  })
-  console.log(result)
+export async function signInWithEmailAndPassword(
+  previousState: unknown,
+  data: FormData,
+) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+    return { success: false, message: null, errors }
+  }
+  const { email, password } = result.data
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+      return { success: false, message, errors: null }
+    }
+
+    console.error(error)
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+  return { success: true, message: null, errors: null }
 }
