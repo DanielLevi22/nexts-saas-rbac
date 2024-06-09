06c913c9e534801fcc5703cf258ea40c23f83715
Daniel Levi <daniellevi000@gmaiil.com>
2024-06-08 21:06:58 -0300

    feat: create verify cargs

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,16 +2,20 @@ import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import logo from '@/assets/logo.svg'
+import { ability } from '@/auth/auth'
 
 import { OrganizationSwitch } from './organization-switcher'
 import { ProfileButton } from './profile-button'
-export function Header() {
+export async function Header() {
+  const permissions = await ability()
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={logo} alt="logo" className="size-6 dark:invert" />
         <Slash className="size-3 -rotate-[24deg] text-border" />
         <OrganizationSwitch />
+
+        {permissions?.can('get', 'Project') && <p>projetos</p>}
       </div>
       <div className="flex items-center gap-4">
         <ProfileButton />
