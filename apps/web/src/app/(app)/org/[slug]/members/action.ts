9b1d999c7faeec23c5bc2a091239dc4cae8e95a4
Daniel Levi <daniellevi000@gmail.com>
2024-07-20 19:15:48 -0300

    feat: update member select

--- apps/web/src/app/(app)/org/[slug]/members/action.ts
+++ apps/web/src/app/(app)/org/[slug]/members/action.ts
@@ -1,14 +1,28 @@
 'use server'
 
+import { Role } from '@saas/auth'
 import { revalidateTag } from 'next/cache'
 
 import { getCurrentOrg } from '@/auth/auth'
 import { removeMember } from '@/http/remove-member'
+import { updateMember } from '@/http/update-member'
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = getCurrentOrg()
 
   await removeMember({
+    org: currentOrg!,
+    memberId,
+  })
+
+  revalidateTag(`${currentOrg}/members`)
+}
+
+export async function updateMemberAction(memberId: string, role: Role) {
+  const currentOrg = getCurrentOrg()
+
+  await updateMember({
+    role,
     org: currentOrg!,
     memberId,
   })
